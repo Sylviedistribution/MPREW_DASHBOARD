cca512cceb082970153f29e4a6f332616b1e96f1
Sylvestre <ibombogakossosylvestre@gmail.com>
2024-12-23 19:29:13 +0000

    Version finale alpha1.2

--- app/Http/Controllers/Api/Commande.php
+++ app/Http/Controllers/Api/Commande.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\CommandeArticles;
 use App\Models\Commandes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -17,7 +18,7 @@ class Commande extends Controller
         Log::info('Client authentifié : ', [$client]);
 
         // Récupérer les robes du client
-        $commandes = Commandes::where('clientId', $client->id)->get(); // Récupère les commandes associées au client
+        $commandes = Commandes::where('clientId', $client->id)->with(['comm'])->get(); // Récupère les commandes associées au client
 
         if ($commandes->isEmpty()) {
             return response()->json([
@@ -28,8 +29,8 @@ class Commande extends Controller
 
         return response()->json([
             'success' => true,
-            'data' => [{"id": 11, "date": 2024 - 12 - 22, "total": 100, "statut": EN_ATTENTE, "clientId": 9, "artisanId": null, "created_at": 2024 - 12 - 22T19:31:27.000000Z, "updated_at": 2024 - 12 - 22T19:31:27.000000Z}]$commandes
-        ], 200)
+            'data' => $commandes
+        ], 200);
     }
 
 
