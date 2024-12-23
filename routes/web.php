0cb308547e24e6fb48d8088654bd40e4988ff91a
Sylvestre <ibombogakossosylvestre@gmail.com>
2024-12-23 19:08:49 +0000

    Version finale alpha1.1

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/', function () {
 
 Route::get('index', function () {
     return view('index');
-})->name('index');
+})->name('index')->middleware('admin');
 
 
 Route::middleware('admin')->prefix('clients/')->controller(ClientController::class)->group(function () {
