6212f4f168c51c28426356d1ada3de1ca24cd10d
Sylvestre <ibombogakossosylvestre@gmail.com>
2024-12-26 06:54:31 +0000

    Mes livraisons 2

--- resources/views/commandes/mesCommandes.blade.php
+++ resources/views/commandes/mesCommandes.blade.php
@@ -128,7 +128,7 @@
                                                      data-title="Livrer robe"
                                                      data-use-location="true"
                                                      data-app="2187871"
-                                                     data-point-b="{{ $coordinates[$index]['longitude'] }},{{ $coordinates[$index]['latitude'] }}"
+                                                     data-point-b="14.738567,-17.456528"
                                                      data-proxy-url="https://yango.go.link/route?start-lat={start-lat}&start-lon={start-lon}&end-lat={end-lat}&end-lon={end-lon}&adj_adgroup=widget&ref={ref}&adj_t=vokme8e_nd9s9z9&lang=ru&adj_deeplink_js=1&utm_source=widget&adj_fallback=https%3A%2F%2Fyango.com%2Fen_int%2Forder%2F%3Fgfrom%3D{start-lon}%2C{start-lat}%26gto%3D{end-lon}%2C{end-lat}%26ref%3D{ref}"
                                                      data-lang="fr">
                                                 </div>
